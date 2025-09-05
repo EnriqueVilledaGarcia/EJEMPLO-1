2e8ffe75372a198c7a26e29cada9c7a0c298bc20
enrique <l21590309@sjuanrio.tecnm.mx>
2025-09-04 20:49:13 -0600

    Se agrego funcionalidad de crear alumno

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Modelo de Contacto
+class Estudiante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+
+# Crear las tablas si no existen
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 #Ruta principal home
 @app.route('/')
 def index():
-    return 'Hola mundo'
+    return render_template('index.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['correo']
+        telefono = request.form['telefono']
+        nuevo_estudiante = Estudiante(nombre=nombre, apellido=apellido, email=email, telefono=telefono)
+        db.session.add(nuevo_estudiante)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('formulario.html')
